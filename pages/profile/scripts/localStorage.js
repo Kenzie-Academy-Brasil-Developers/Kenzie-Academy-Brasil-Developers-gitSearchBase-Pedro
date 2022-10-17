b5b9a695224491a3c8e56fb05f17efba137a7aa4
Pedro <pedropequera@gmail.com>
2022-10-17 00:29:19 -0300

    Git Search

---
+++ pages/profile/scripts/localStorage.js
@@ -0,0 +1,21 @@
+let user = {}
+let repos = []
+
+function getUserNow() {
+    const getJson = localStorage.getItem("userNow")
+
+    if (getJson){
+        user = JSON.parse(getJson)
+    }
+}
+
+function getRepoNow() {
+    const getJson = localStorage.getItem("repoNow")
+
+    if (getJson){
+        repos = JSON.parse(getJson)
+    }
+}
+
+getUserNow()
+getRepoNow()
